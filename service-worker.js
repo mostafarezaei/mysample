0835826fa5947a3712e7b9bee52ffbcee334c196
mostafa <mostafa.internet@ymail.com>
2015-07-26 10:16:36 +0430

    new changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('lovefield.js', 'require.js', 'require.config.js');
+importScripts('ydn.js', 'require.js', 'require.config.js');
 
 self.addEventListener('install', function(e) {});
 
@@ -6,16 +6,5 @@ self.addEventListener('activate', function(e) {});
 
 self.addEventListener('message', function(event) {
  debugger;
- var schemaBuilder = lf.schema.create('crdb', 1);
- schemaBuilder.createTable('Asset').
- addColumn('id', lf.Type.STRING).
- addColumn('asset', lf.Type.STRING).
- addColumn('timestamp', lf.Type.INTEGER).
- addPrimaryKey(['id']);
-
- // Schema is defined, now connect to the database instance.
- schemaBuilder.connect().then(
-  function(db) {
-   debugger;
-  });
+ 
 });
