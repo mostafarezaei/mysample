7e31336767b5410378347cb71e67cfa7f91b29d2
mostafa <mostafa.internet@ymail.com>
2015-07-26 10:20:20 +0430

    new changes

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
 importScripts('ydn.js', 'require.js', 'require.config.js');
+debugger;
+self.addEventListener('install', function(e) {
+ debugger;
+});
 
-self.addEventListener('install', function(e) {});
-
-self.addEventListener('activate', function(e) {});
+self.addEventListener('activate', function(e) {
+ debugger;
+});
 
 self.addEventListener('message', function(event) {
  debugger;
- 
 });
