b29ceb18a09950fd99ded461f0e426d7ea71a327
mostafa <mostafa.internet@ymail.com>
2015-07-27 09:33:48 +0430

    new changes

---
+++ shared-worker.js
@@ -0,0 +1,32 @@
+importScripts(
+ '${stdlib.getStaticContentUrl("/js/plugins/require/require.js")}',
+ '${stdlib.getStaticContentUrl("/js/require.config.js")}'
+);
+
+var lastNumber = null;
+
+onconnect = function(e) {
+ var port = e.ports[0];
+ port.onmessage = handleMessages(e);
+ port.start();
+}
+
+function handleMessages(event) {
+ debugger;
+ require(['db'], function(db) {
+  debugger;
+  //ws.ensureServerConnectivity();
+  console.log('Handling message event:', event);
+  switch (event.data.command) {
+   case 'run':
+    event.ports[0].postMessage({
+     error: null,
+     response: 'this is your response3!!'
+    });
+    break;
+   default:
+    // This will be handled by the outer .catch().
+    throw 'Unknown command: ' + event.data.command;
+  }
+ });
+};
