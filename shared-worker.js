635c0361d8c54feed0ff3c61d11e9a657d57c9f1
mostafa <mostafa.internet@ymail.com>
2015-07-27 09:47:35 +0430

    new changes

--- shared-worker.js
+++ shared-worker.js
@@ -1,32 +1,29 @@
-importScripts(
- '${stdlib.getStaticContentUrl("/js/plugins/require/require.js")}',
- '${stdlib.getStaticContentUrl("/js/require.config.js")}'
-);
+importScripts('require.js', 'require.config.js');
+debugger;
 
 var lastNumber = null;
 
 onconnect = function(e) {
- var port = e.ports[0];
- port.onmessage = handleMessages(e);
- port.start();
+ require(['ydn'], function(ydn) {
+  debugger;
+  var port = e.ports[0];
+  port.onmessage = handleMessages(e, ydn);
+  port.start();
+ });
 }
 
-function handleMessages(event) {
- debugger;
- require(['db'], function(db) {
-  debugger;
-  //ws.ensureServerConnectivity();
-  console.log('Handling message event:', event);
-  switch (event.data.command) {
-   case 'run':
-    event.ports[0].postMessage({
-     error: null,
-     response: 'this is your response3!!'
-    });
-    break;
-   default:
-    // This will be handled by the outer .catch().
-    throw 'Unknown command: ' + event.data.command;
-  }
- });
+function handleMessages(event, ydn) {
+ //ws.ensureServerConnectivity();
+ console.log('Handling message event:', event);
+ switch (event.data.command) {
+  case 'run':
+   event.ports[0].postMessage({
+    error: null,
+    response: 'this is your response3!!'
+   });
+   break;
+  default:
+   // This will be handled by the outer .catch().
+   throw 'Unknown command: ' + event.data.command;
+ }
 };
